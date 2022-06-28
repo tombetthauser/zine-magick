33ff515fa4fb44fa4e10dcc350704f8ee8b9336b
tombetthauser <tombetthauser@gmail.com>
2022-06-28 11:13:58 -0700

    add cleaned up half page horizontal

--- scripts/half-page-vertical-zine-cleaned-up.sh
+++ scripts/half-page-vertical-zine-cleaned-up.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Welcome, this is a time-saving tool for making zines!
 
 # It does the following...
@@ -19,7 +21,7 @@
 # ---------------------------------------------------------------------------
 
 # show all activity in shell
-set -x
+if [[ $1 = "-d" || $1 = "-debug" ]]; then set -x; fi
 
 # reset directories and copy zine images
 rm -rf ./zine-images ./zine-pages ./output
@@ -34,13 +36,14 @@ imageCount=${#zineImageFileNames[@]}
 pageMax=2; sheetMax=4
 
 # calculate number of pages needed rounding up
-pagesNeeded=$(((imageCount / sheetMax) * 2))
+paesNeeded=$(((imageCount / sheetMax) * 2))
 if [ $(($imageCount % $sheetMax)) -gt 1 ]; then pagesNeeded=$((pagesNeeded+2))
 elif [ $(($imageCount % $sheetMax)) -gt 0 ]; then pagesNeeded=$((pagesNeeded+1))
 fi
 
 # set output page resolution in pixels
 pageWidth=850; pageHeight=1100
+# pageWidth=1700; pageHeight=2200
 
 
 # ---------------------------------------------------------------------------
@@ -49,27 +52,51 @@ pageWidth=850; pageHeight=1100
 replaceColorWhite=false
 blackReplacement=false
 monochromeDither=false
+orientation=vertical
 colorDither=false
 traceEdges=false
 monochrome=false
 posterize=false
 dither=false
 invert=false
+size=small
+output=pdf
+rotate=0
 
 
 # take optional styling arguments from command line 
 for arg in "$@"
 do
-  if [[ $arg = resolution:* ]]; then xPixelsTemp=(${arg//:/ }); pageWidth=${xPixelsTemp[1]}; pageHeight=${xPixelsTemp[2]}; fi
-  if [[ $arg = white:* ]]; then tempColor=(${arg//:/ }); replaceColorWhite=${tempColor[1]}; fi
-  if [[ $arg = black:* ]]; then blackReplacement=${tempColor#black:}; fi
-  if [[ $arg = monochrome-dither:* ]]; then monochromeDither=true; fi
-  if [[ $arg = color-dither:* ]]; then colorDither=true; fi
-  if [[ $arg = trace-edges:* ]]; then traceEdges=true; fi
-  if [[ $arg = monochrome:* ]]; then monochrome=true; fi
-  if [[ $arg = posterize:* ]]; then posterize=true; fi
-  if [[ $arg = invert:* ]]; then invert=true; fi # <--- why is [[ x = x ]] necessary instead of [[ x -eq x ]] or [ x -eq x ]
+  if [[ $arg = resolution* ]]; then xPixelsTemp=(${arg//:/ }); pageWidth=${xPixelsTemp[1]}; pageHeight=${xPixelsTemp[2]}; fi
+  if [[ $arg = white* ]]; then tempColor=(${arg//:/ }); replaceColorWhite=${tempColor[1]}; fi
+  if [[ $arg = orientation* ]]; then orientation=${arg#orientation:}; fi
+  if [[ $arg = monochrome-dither* ]]; then monochromeDither=true; fi
+  if [[ $arg = black* ]]; then blackReplacement=${arg#black:}; fi
+  if [[ $arg = color-dither* ]]; then colorDither=true; fi
+  if [[ $arg = rotate* ]]; then rotate=${arg#rotate:}; fi
+  if [[ $arg = output* ]]; then output=${arg#output:}; fi
+  if [[ $arg = trace-edges* ]]; then traceEdges=true; fi
+  if [[ $arg = monochrome* ]]; then monochrome=true; fi
+  if [[ $arg = posterize* ]]; then posterize=true; fi
+  if [[ $arg = size* ]]; then size=${arg#size:}; fi
+  if [[ $arg = invert* ]]; then invert=true; fi # <--- why is [[ x = x ]] necessary instead of [[ x -eq x ]] or [ x -eq x ]
 done
+
+
+# ---------------------------------------------------------------------------
+
+# create blank page with color to prevent mono defaulting
+if [ $size = extrasmall ]
+then pageWidth=212; pageHeight=275
+elif [ $size = small ]
+then pageWidth=425; pageHeight=550
+elif [ $size = medium ]
+then pageWidth=850; pageHeight=1100
+elif [ $size = large ]
+then pageWidth=1275; pageHeight=1650
+elif [ $size = extralarge ]
+then pageWidth=1700; pageHeight=2200
+fi
 
 
 # ---------------------------------------------------------------------------
@@ -93,15 +120,34 @@ yImagePercent=4773 # <-- represents 47.73%
 imageHeight=$(((xImagePercent * pageWidth) / 10000)) # <-- division simulates percentage
 imageWidth=$(((yImagePercent * pageHeight) / 10000))
 
+if [ $orientation = horizontal ] 
+then
+  temp=$imageHeight
+  imageHeight=$imageWidth
+  imageWidth=$temp
+fi
+
 
 # resize and then style all individual image files
 for ((i=0; i<$((imageCount)); i++)); do
 
   # capture image filename and path
-  zineImage=${zineImageFileNames[$((i))]}
-  
+  zineImage=${zineImageFileNames[i]}
+
   # resize image
-  convert $zineImage -resize $((imageWidth))x$((imageHeight))^ -gravity center -extent $((imageWidth))x$((imageHeight)) $zineImage
+  # convert $zineImage -resize $((imageWidth))x$((imageHeight))^ -gravity center -extent $((imageWidth))x$((imageHeight)) $zineImage
+
+  convert $zineImage -resize $((imageWidth))x$((imageHeight))^ -gravity center -extent $((imageWidth))x$((imageHeight)) ./zine-images/$i-image.png
+
+  # if [ $orientation = vertical ]
+  # then convert $zineImage -resize $((imageWidth))x$((imageHeight))^ -gravity center -extent $((imageWidth))x$((imageHeight)) ./zine-images/$i-image.png
+  # else convert $zineImage -resize $((imageHeight))x$((imageWidth))^ -gravity center -extent $((imageHeight))x$((imageWidth)) ./zine-images/$i-image.png
+  # fi
+
+  # convert $zineImage -resize $((imageHeight))x$((imageWidth))^ -gravity center -extent $((imageHeight))x$((imageWidth)) ./zine-images/$i-image.png
+  rm $zineImage
+
+  zineImage=./zine-images/$i-image.png
 
   # apply all imagemagick styles
   if [ $monochrome = true ]; then convert $zineImage -colorspace Gray $zineImage; fi
@@ -110,15 +156,31 @@ for ((i=0; i<$((imageCount)); i++)); do
   if [ $invert = true ]; then convert $zineImage -negate $zineImage; fi
   if [ $monochromeDither = true ]; then convert $zineImage -monochrome $zineImage; fi
   if [ $colorDither = true ]; then convert $zineImage -channel RGBA -separate +clone -dither FloydSteinberg -remap pattern:gray50 +swap +delete -combine $zineImage; fi
-  if ! [ $replaceColorWhite = false ]; then convert $zineImage -colorspace sRGB -fuzz 33% -fill $replaceColorWhite -opaque white $zineImage; fi
-  if ! [ $blackReplacement = false ]; then convert $zineImage -colorspace sRGB -fuzz 33% -fill $blackReplacement -opaque black $zineImage; fi
+  if [ ! $replaceColorWhite = false ]; then convert $zineImage -colorspace sRGB -fuzz 33% -fill $replaceColorWhite -opaque white $zineImage; fi
+  if [ ! $blackReplacement = false ]; then convert $zineImage -colorspace sRGB -fuzz 33% -fill $blackReplacement -opaque black $zineImage; fi
 done
+
+# reset file names array
+zineImageFileNames=(./zine-images/*)
+
 
 
 # ---------------------------------------------------------------------------
 
 # static image rotation pattern for proper print orientations
-rotations=(270 90 90 270) # <--- represent clockwise degrees
+# rotations=(270 90 90 270) # <--- represent clockwise degrees
+
+# set rotation patterns based on orientation
+if [[ $orientation == horizontal ]]
+then rotations=(0 0 180 180) # <--- represent clockwise degrees
+else rotations=(270 90 90 270) # <--- represent clockwise degrees
+fi
+
+# apply optional rotation
+for ((i=0; i<${#rotations[@]}; i++))
+do rotations[$i]=$(((rotations[i] + rotate) % 360))
+done
+
 
 # grab length of rotations dynamically for longer patterns
 rotationsLength=${#rotations[@]}
@@ -147,7 +209,7 @@ then
   convert -size 1x1 xc:lime ./zine-placeholders/1001-ordered.png
 
   # copy placeholder image and name for proper unix ordering
-  for ((i=1; i<((imageCountRoundedUp)); i++)); do
+  for ((i=1; i<imageCountRoundedUp; i++)); do
     cp ./zine-placeholders/1001-ordered.png ./zine-placeholders/$((i + 1 + 1000))-ordered.png
   done
 fi
@@ -164,8 +226,8 @@ maxImagePlacementNumber=1
 
 
 # rename all zine image files for correct order
-for ((i=0; i<$((imageCount)); i++)); do
-  zineImage=${zineImageFileNames[$((i))]}
+for ((i=0; i<$imageCount; i++)); do
+  zineImage=${zineImageFileNames[i]}
 
   relativeOrder=${relativePageOrders[$((i % relativePageOrdersLength))]}
   
@@ -174,8 +236,7 @@ for ((i=0; i<$((imageCount)); i++)); do
 
   # update maxImagePlacementNumber
   if [ $absoluteOrder -gt $maxImagePlacementNumber ]
-  then
-    maxImagePlacementNumber=$absoluteOrder
+  then maxImagePlacementNumber=$absoluteOrder
   fi
   
   # trim off the file extension
@@ -233,16 +294,29 @@ done
 
 # ---------------------------------------------------------------------------
 
+# recollect page file names
+pageFiles=(./zine-pages/*)
+pageCount=${#pageFiles[@]}
+
+# replace placeholder lime color with white on all pages
+for ((i=0; i<$((pageCount)); i++)); do
+  pageFile=${pageFiles[$((i))]}
+  convert $pageFile -colorspace sRGB -fuzz 1% -fill white -opaque lime $pageFile
+done
+
+
+# ---------------------------------------------------------------------------
+
 # combine all image files into pdf
-magick convert ./zine-pages/* ./output/zine.pdf
+magick convert ./zine-pages/* ./output/zine.$output
 
 # replace limegreen placeholder color
-convert ./output/zine.pdf -colorspace sRGB -fuzz 1% -fill white -opaque lime ./output/zine.pdf
+# convert ./output/zine.$output -colorspace sRGB -fuzz 1% -fill white -opaque lime ./output/zine.$output
 
 # clean up temp directories and files
 rm -rf ./zine-placeholders
 rm -rf ./zine-images
-rm -rf ./zine-pages
+# rm -rf ./zine-pages
 
 # open the finished zine file
 open ./output/*
